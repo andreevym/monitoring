b303fd3349ca4df936796782c272b866bbcfef04
Yury Andreev <Yury.Andreev@ru.ibm.com>
2017-02-13 02:59:38 +0300

    добавил борду для метода getAuditHistory

--- config/grafana.cmd
+++ config/grafana.cmd
@@ -4,3 +4,4 @@ apt-get update
 apt-get -y install curl
 curl -v POST http://grafana:3000/api/datasources -d @/etc/grafana/scripts/datasource.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
 curl -X POST http://grafana:3000/api/dashboards/db -d @/etc/grafana/scripts/dashboard.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
+curl -X POST http://grafana:3000/api/dashboards/db -d @/etc/grafana/scripts/dashboard-getAuditHistory.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
