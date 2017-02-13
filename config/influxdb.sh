db5d9d3b98483966655ac6cc1d7a6c32e7ec38b8
Yury Andreev <Yury.Andreev@ru.ibm.com>
2017-02-13 17:38:14 +0300

    SHOW MEASUREMENTS

--- config/influxdb.sh
+++ config/influxdb.sh
@@ -10,5 +10,9 @@ influx -execute 'SHOW DATABASES'
 
 echo ">>>>> SHOW USERS <<<<<"
 influx -database 'hyperledger' -execute 'SHOW USERS'
+
+echo ">>>>> SHOW MEASUREMENTS <<<<<"
+influx -database 'hyperledger' -execute 'SHOW MEASUREMENTS'
+
 echo ">>>>> CREATE USER admin <<<<<"
 influx -database 'hyperledger' -execute "CREATE USER 'admin' WITH PASSWORD 'admin' WITH ALL PRIVILEGES"
