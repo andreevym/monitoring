6d8286086ebdf4604de94586a296ec250dc7da6a
andreevym <Yury.Andreev@ru.ibm.com>
2017-02-14 15:18:52 +0300

    remove unused

--- config/grafana.cmd
+++ config/grafana.cmd
@@ -8,7 +8,3 @@ echo ">>>>> setup datasources <<<<<"
 curl -v POST http://grafana:3000/api/datasources -d @/etc/grafana/scripts/datasource.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
 echo ">>>>> setup dashboards - Hyperledger <<<<<"
 curl -X POST http://grafana:3000/api/dashboards/db -d @/etc/grafana/scripts/dashboard.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
-echo ">>>>> setup dashboards - getAuditHistory <<<<<"
-curl -X POST http://grafana:3000/api/dashboards/db -d @/etc/grafana/scripts/dashboard-getAuditHistory.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
-echo ">>>>> setup dashboards - main <<<<<"
-curl -X POST http://grafana:3000/api/dashboards/db -d @/etc/grafana/scripts/dashboard-main.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
